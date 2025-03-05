00150fc806ce86572afffce1b4791bf0dde7f2f5
Mrsangmin <sangmin0280@naver.com>
2025-03-05 11:35:18 +0900

    오상민
    GameInstance, Gamemode 업데이트

--- CG_Project/Source/CG_Project/Private/CG_GameMode.cpp
+++ CG_Project/Source/CG_Project/Private/CG_GameMode.cpp
@@ -1,5 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CG_GameMode.h"
+#include "CG_GameState.h"
 
+ACG_GameMode::ACG_GameMode()
+{
+	GameStateClass = ACG_GameState::StaticClass();
+}
--- CG_Project/Source/CG_Project/Public/CG_GameInstance.h
+++ CG_Project/Source/CG_Project/Public/CG_GameInstance.h
@@ -1,17 +1,23 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "CG_GameInstance.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class CG_PROJECT_API UCG_GameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+	UCG_GameInstance();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "GameData")
+	int32 TotalScore; //총점
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "GameData")
+	int32 CurrentLevelIndex; //현재 레벨
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	void AddToScore(int32 Amount); //점수 추가
+
 };
