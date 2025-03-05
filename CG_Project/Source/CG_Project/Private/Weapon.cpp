852ce704c7011637a67b57e584f93edc637b3519
yeouDend <144914292+yeouDend@users.noreply.github.com>
2025-03-05 14:18:18 +0900

    Gun
    123

--- CG_Project/Source/CG_Project/Private/Weapon.cpp
+++ CG_Project/Source/CG_Project/Private/Weapon.cpp
@@ -1,27 +1,50 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Weapon.h"
 
-// Sets default values
+
 AWeapon::AWeapon()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	SetRootComponent(RootComp);
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	WeaponMesh->SetupAttachment(RootComp);
+
+	DamageAmount = 20.0f;
+	WeaponRange = 10000.0f;
 }
 
-// Called when the game starts or when spawned
-void AWeapon::BeginPlay()
+void AWeapon::Fire()
 {
-	Super::BeginPlay();
-	
+	FVector StartLocation = GetOwner()->GetActorLocation();
+	FVector ForwardVector = GetOwner()->GetActorForwardVector();
+	FVector EndLocation = StartLocation + (ForwardVector * WeaponRange);
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(this);
+	QueryParams.AddIgnoredActor(GetOwner());
+	QueryParams.bTraceComplex = true;
+
+	FHitResult HitResult;
+	bool bIsHit = GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams);
+	if (bIsHit)
+	{
+		AActor* HitActor = HitResult.GetActor();
+		if (HitActor)
+		{
+			FDamageEvent DamageEvent;
+			HitActor->TakeDamage(DamageAmount, DamageEvent, nullptr, this);
+			UE_LOG(LogTemp, Warning, TEXT("Hit Actor: %s, Applied Damage: %.1f"), *HitActor->GetName(), DamageAmount);
+		}
+
+		DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 16, FColor::Green, false, 2.0f);
+		DrawDebugLine(GetWorld(), StartLocation, HitResult.ImpactPoint, FColor::Green, false, 2.0f, 0, 1.0f);
+	}
+	else
+	{
+		DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 2.0f, 0, 1.0f);
+	}
 }
 
-// Called every frame
-void AWeapon::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
