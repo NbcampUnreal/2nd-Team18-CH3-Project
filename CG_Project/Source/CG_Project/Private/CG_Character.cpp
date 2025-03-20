a11e6e8c44b6e13eb19135a5a1c8612998a79394
yeouDend <144914292+yeouDend@users.noreply.github.com>
2025-03-20 11:08:30 +0900

    01

--- CG_Project/Source/CG_Project/Private/CG_Character.cpp
+++ CG_Project/Source/CG_Project/Private/CG_Character.cpp
@@ -15,34 +15,34 @@ ACG_Character::ACG_Character()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	// 카메라 조정 막대 관련
+	// ???? ???? ???? ????
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArmComp->SetupAttachment(RootComponent);
 	SpringArmComp->TargetArmLength = 300.0f;
 	SpringArmComp->bUsePawnControlRotation = true;
 
-	// 카메라
+	// ????
 	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 	CameraComp->bUsePawnControlRotation = false;
 
-	// 머리 위 HP 관련 위젯
+	// ??? ?? HP ???? ????
 	OverheadWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("OverheadWidget"));
 	OverheadWidget->SetupAttachment(GetMesh());
 	OverheadWidget->SetWidgetSpace(EWidgetSpace::Screen);
 
-	// 플레이어 이동속도
+	// ?÷???? ??????
 	NormalSpeed = 350.0f;
 	SprintSpeedMultiplier = 1.8f;
 	SprintSpeed = NormalSpeed * SprintSpeedMultiplier;
 
 	GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
 
-	// 캐릭터 체력
+	// ĳ???? ???
 	MaxHealth = (CurrentLevel * 10.0f) + 90.0f;
 	CurrentHealth = MaxHealth;
 
-	// 캐릭터 레벨 
+	// ĳ???? ???? 
 	CurrentLevel = 1;
 }
 
@@ -53,7 +53,7 @@ void ACG_Character::BeginPlay()
 
 	if (StickyGunClass)
 	{
-		EquipWeapon(); // 무기 소환
+		EquipWeapon(); // ???? ???
 	}
 	else
 	{
@@ -61,7 +61,7 @@ void ACG_Character::BeginPlay()
 	}
 }
 
-// 캐릭터 조작 관련
+// ĳ???? ???? ????
 void ACG_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
@@ -153,7 +153,7 @@ void ACG_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 	}
 }
 
-// 이동
+// ???
 void ACG_Character::Move(const FInputActionValue& value)
 {
 	if (!Controller) return;
@@ -171,7 +171,7 @@ void ACG_Character::Move(const FInputActionValue& value)
 	}
 }
 
-// 점프(시작)
+// ????(????)
 void ACG_Character::StartJump(const FInputActionValue& value)
 {
 	if (value.Get<bool>())
@@ -180,7 +180,7 @@ void ACG_Character::StartJump(const FInputActionValue& value)
 	}
 }
 
-// 점프(멈춤)
+// ????(????)
 void ACG_Character::StopJump(const FInputActionValue& value)
 {
 	if (!value.Get<bool>())
@@ -189,7 +189,7 @@ void ACG_Character::StopJump(const FInputActionValue& value)
 	}
 }
 
-// 시점
+// ????
 void ACG_Character::Look(const FInputActionValue& value)
 {
 	FVector2D LookInput = value.Get<FVector2D>();
@@ -198,7 +198,7 @@ void ACG_Character::Look(const FInputActionValue& value)
 	AddControllerPitchInput(LookInput.Y);
 }
 
-// 달리기(시작)
+// ?????(????)
 void ACG_Character::StartSprint(const FInputActionValue& value)
 {
 	if (GetCharacterMovement())
@@ -207,7 +207,7 @@ void ACG_Character::StartSprint(const FInputActionValue& value)
 	}
 }
 
-// 달리기(멈춤)
+// ?????(????)
 void ACG_Character::StopSprint(const FInputActionValue& value)
 {
 	if (GetCharacterMovement())
@@ -217,7 +217,7 @@ void ACG_Character::StopSprint(const FInputActionValue& value)
 }
 
 
-// 구르기 (우클릭)
+// ?????? (?????)
 void ACG_Character::Roll(const FInputActionValue& value)
 {
 	if (value.Get<bool>())
@@ -226,65 +226,65 @@ void ACG_Character::Roll(const FInputActionValue& value)
 	}
 }
 
-// 캐릭터 머리 위 hp바
+// ĳ???? ??? ?? hp??
 void ACG_Character::UpdateOverheadHP()
 {
-	// 추가해야됨
+	// ???????
 }
 
-// 현재 체력 get함수
+// ???? ??? get???
 float ACG_Character::GetHealth() const
 {
 	return CurrentHealth;
 }
 
-// 체력 회복 매커니즘
+// ??? ??? ??Ŀ????
 void ACG_Character::HealHealth(float Amount)
 {
-	// Clamp(최소~최대 체력 사이의 값, 최소체력, 최대체력)
+	// Clamp(???~??? ??? ?????? ??, ??????, ??????)
 	CurrentHealth = FMath::Clamp(CurrentHealth + Amount, 0.0f, MaxHealth);
-	// 계산이 끝난 뒤 체력바 위젯에 업데이트
+	// ????? ???? ?? ??¹? ?????? ???????
 	UpdateOverheadHP();
 }
 
-//// 캐릭터 데미지 공식(레벨 비례 데미지 증가 포함해야 함)
+//// ĳ???? ?????? ????(???? ??? ?????? ???? ??????? ??)
 //float ACG_Character::DealDamage()
 //{
-//	// 추가해야됨
+//	// ???????
 // 
 //}
 //
-//// 캐릭터 받는 데미지 공식
+//// ĳ???? ??? ?????? ????
 //float ACG_Character::TakeDamage()
 //{
-//	// 추가해야됨
+//	// ???????
 //}
 
-// 캐릭터 사망시 이벤트(게임오버 등)
+// ĳ???? ????? ????(??????? ??)
 void ACG_Character::OnDeath()
 {
-	// 추가해야됨
+	// ???????
 }
 
 void ACG_Character::EquipWeapon()
 {
-	// 액터 생성 파라미터 설정
+	// ???? ???? ?????? ????
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.Owner = this;
 	SpawnParams.Instigator = GetInstigator();
 
-	// Weapon 액터 소환
+	// Weapon ???? ???
 	AActor* NewWeapon = GetWorld()->SpawnActor<AActor>(StickyGunClass, GetActorLocation(), GetActorRotation(), SpawnParams);
 
 	if (NewWeapon)
 	{
-		// 인터페이스 획득 시도
+		// ????????? ??? ???
 		IWeaponInterface* InterfaceWeapon = Cast<IWeaponInterface>(NewWeapon);
 
 		if (InterfaceWeapon)
 		{
-			// 무기 부착
-			(Cast<AActor>(InterfaceWeapon))->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("R_thumb_02_s_073Socket")); // 소켓에 부착
+			// ???? ????
+			(Cast<AActor>(InterfaceWeapon))->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, FName("R_thumb_02_s_073Socket")); // ????? ????
 			EquippedWeapon = TScriptInterface<IWeaponInterface>(NewWeapon);
 		}
 	}
@@ -294,7 +294,7 @@ void ACG_Character::Fire()
 {
 	float CurrentTime = GetWorld()->GetTimeSeconds();
 
-	// Critical Section 시작 (LastFireTime 접근 전에)
+	// Critical Section ???? (LastFireTime ???? ????)
 	FireTimeSection.Lock();
 	
 	if (EquippedWeapon.GetObject())
@@ -302,9 +302,9 @@ void ACG_Character::Fire()
 		IWeaponInterface* Weapon = Cast<IWeaponInterface>(EquippedWeapon.GetObject());
 		if (Weapon)
 		{
-			Weapon->Fire(); // 인터페이스를 통해 Fire 함수 직접 호출
+			Weapon->Fire(); // ??????????? ???? Fire ??? ???? ???
 		}
 	}
-	// Critical Section 종료 (LastFireTime 접근 후에)
+	// Critical Section ???? (LastFireTime ???? ?Ŀ?)
 	FireTimeSection.Unlock();
 }
